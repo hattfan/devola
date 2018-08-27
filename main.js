af8175a6c79978e7a755afd4178432575290e090
Ola Karlsson <ola.karlsson@aol.com>
2018-08-27 13:22:15 +0000

    Aceess control change

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ app.use(express.static(__dirname + '/views'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3030');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
