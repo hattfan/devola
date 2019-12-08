cf53cef3d15ba9f46f7aca8071290aefdea88f1a
unknown <ola.anders.karlsson@gmail.com>
2019-12-08 21:17:19 +0100

    la till carlpong

---
+++ views/carlpong/js/mongo/nyaslumpen.py
@@ -0,0 +1,19 @@
+############################################################################
+#? Uppdaterar in s.a. nyaslumpen har distinkt vilka som ska slumpas ########
+############################################################################
+
+import pymongo
+
+#!Definition of mongo
+myclient = pymongo.MongoClient("mongodb://ola:Neroxrox5(@ds121861.mlab.com:21861/foosball")
+mydb = myclient['foosball']
+mycol = mydb["aktiva"]
+
+#!database-findings
+dbdata = mycol.find()
+print('Running')
+
+# För varje artikel, kör loopen
+for row in dbdata:
+    print(row)
+    mycol.update_one({$set:{'Aktiv':true}})
